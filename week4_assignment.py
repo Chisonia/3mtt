a92d7bf8a1725ce96268b36d8721051b58b3d8fc
Chisonia <sonya12slyvia@gmail.com>
2024-09-21 16:13:00 +0100

    first commit

---
+++ week4_assignment.py
@@ -0,0 +1,12 @@
+#Write a Python script that takes two numbers as input and performs basic
+#arithmetic operations (addition, subtraction, multiplication, division
+
+def add(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a - b)
+
+add(10, 2)
+
+
